7912a050cd38a3ee394b6e6ee870a6d11bd80b53
Thiago <thiagoferreira.dev@gmail.com>
2020-05-24 11:03:20 -0300

    criando header component

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/componentes/Header';
+import axios from 'axios';
 
 export default class App extends React.Component {
     constructor(props) {
       super(props);
       
+    }
+
+    componentDidMount() {
+      axios.get('https://api.giphy.com/v1/gifs/trending?api_key=k4GKQO8QhDnXYdswOdLoSGoXvmuDHjdV&limit=1')
+        .then(response => {
+          const { data } = response.data;
+          console.log(data)
+        });
     }
 
 
--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -3,12 +3,26 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const Header = (props) => {
     return (
-        <View>
-            <Text>
+        <View style={style.container}>
+            <Text  style={style.title}>
                {props.title}
             </Text>
         </View>
     );
 }
+
+const style =  StyleSheet.create({
+    container: {
+		marginTop: 25,
+		backgroundColor: 'black',
+
+		alignItems: 'center',
+        justifyContent: 'center'
+	},
+	title: {
+		fontSize: 50,
+		color: '#fff',
+	}
+})
 
 export default Header;
